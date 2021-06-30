43cfecd2566a2a25f27982e881847cfd4699b821
SigCavallo10 <60301963+SigCavallo10@users.noreply.github.com>
2021-06-30 03:44:28 +0200

    Update articoli.php

--- articoli.php
+++ articoli.php
@@ -9,15 +9,17 @@ if(mysqli_num_rows($result) > 0){?>
 <?php
     while($riga = mysqli_fetch_assoc($result)){?>
     <div class="articolo">
-        <div>
-            <h1><?php echo $riga['titolo']?></h1>    
+        <div class="head">
+            <h1><?php echo $riga['titolo']?></h1>
+    </div>
+    <div class="info"> 
             <h4><?php echo $riga['cognome']." ".$riga['nome']?></h4>
             <h4><?php echo $riga['tempo']?></h4>
         </div>
-        <div>
+        <div class="testo">
             <p><?php echo substr($riga['testo'], 0, 100)?></p>
         </div>
-        <div>
+        <div class="footer">
             <form action="articolo.php" method="GET">
                 <input type="hidden" name="articolo" value="<?php echo $riga['id'] ?>">
                 <button type="submit">leggi di più</button>
@@ -33,10 +35,27 @@ if(mysqli_num_rows($result) > 0){?>
     .articoli{
         display: flex;
         flex-direction: row;
+        flex-wrap: wrap;
     }
     .articolo{
-        margin: 50px 20px;
-        width: 200px;
-        height: 300px;
+        flex: 0 0 30%;
+        margin: 20px;
+        padding: 20px;
+        border: 3px solid black;
+    }
+    @media only screen and (max-width: 768px) {
+        .articolo {
+            flex: 0 0 50%;
+        }
+    }
+    @media only screen and (max-width: 641px) {
+        .articolo {
+            flex: 0 0 80%;
+        }
+    }
+    .info{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
     }
 </style>
